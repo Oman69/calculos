e22b9966e4350525a8b6aaeddfb3f07d5a75d2c0
oman69 <kazyk-89@mail.ru>
2025-02-13 09:08:05 +0300

    add images

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Kazickiirs\Desktop\PycharmProjects\calculos\.venv
+set VIRTUAL_ENV=C:\Users\kazyk\PycharmProjects\calculos\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- api.py
+++ api.py
@@ -5,7 +5,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from api_geometry import router as math_router
 
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -17,23 +16,30 @@ app.include_router(math_router)
 def home_page(request: Request, category: int = 1, limit: int = 6):
     headers = {1: 'Геометрические калькуляторы',
                2: 'Финансовые калькуляторы',
-               3: 'Строительные калькуляторы',}
+               3: 'Строительные калькуляторы', }
 
     header_h1 = headers.get(category, 'Заголовок H1')
 
     # ToDo Подключить БД
-    fake_db = [{'category': 1, 'name': 'Площадь круга', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Длина круга', 'slug': 'circle_len'},
-               {'category': 1, 'name': 'Площадь прямоугольника', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Периметр прямоугольника', 'slug': 'circle_len'},
-               {'category': 1, 'name': 'Площадь прямоугольного треугольника', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Гипотенуза прямоугольного треугольника', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Площадь равнобедренного треугольника', 'slug': 'circle_len'},
-               {'category': 1, 'name': 'Площадь равностороннего треугольника', 'slug': 'circle_len'},
-               {'category': 1, 'name': 'Высота равностороннего треугольника', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Площадь квадрата', 'slug': 'circle_area'},
-               {'category': 1, 'name': 'Периметр квадрата', 'slug': 'circle_area'},
-]
+    fake_db = [{'category': 1, 'name': 'Площадь круга', 'slug': 'circle_area', 'image': 'images/circle_s.png'},
+               {'category': 1, 'name': 'Длина круга', 'slug': 'circle_len', 'image': 'images/circle_l.png'},
+               {'category': 1, 'name': 'Площадь прямоугольника', 'slug': 'circle_area', 'image': 'images/pr_s.png'},
+               {'category': 1, 'name': 'Периметр прямоугольника', 'slug': 'circle_len', 'image': 'images/pr_l.png'},
+               {'category': 1, 'name': 'Площадь прямоугольного треугольника', 'slug': 'circle_area',
+                'image': 'images/tr1_s.png'},
+               {'category': 1, 'name': 'Гипотенуза прямоугольного треугольника', 'slug': 'circle_area',
+                'image': 'images/tr1_g.png'},
+               {'category': 1, 'name': 'Площадь равнобедренного треугольника', 'slug': 'circle_len',
+                'image': 'images/tr3_s.png'},
+               {'category': 1, 'name': 'Площадь равностороннего треугольника', 'slug': 'circle_len',
+                'image': 'images/tr2_s.png'},
+               {'category': 1, 'name': 'Высота равностороннего треугольника', 'slug': 'circle_area',
+                'image': 'images/tr2_h.png'},
+               {'category': 1, 'name': 'Площадь квадрата', 'slug': 'circle_area',
+                'image': 'images/kv_s.png'},
+               {'category': 1, 'name': 'Периметр квадрата', 'slug': 'circle_area',
+                'image': 'images/kv_p.png'},
+               ]
 
     filter_data = [i for i in fake_db if i['category'] == category][:limit]
     # Получить данные
