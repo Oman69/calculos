12224b2d5e5b94e3bbf2317e924c4aa86280c026
kazickiirs <kazickiirs>
2025-03-19 18:09:21 +0300

    Начал переписывать на классы

--- geometry/cube.py
+++ geometry/cube.py
@@ -6,24 +6,43 @@ from geometry.models import Cube
 router = APIRouter(prefix='/cube', tags=['Cube'])
 templates = Jinja2Templates(directory="templates")
 
+FIGURE = 'куба'
+H2 = 'найти через ребро'
+
+context_area = {
+        'title': 'Найти площадь ' + FIGURE,
+        'h1': 'Площадь ' + FIGURE,
+        'h2': H2,
+        'h3': 'Площадь ' + FIGURE + ' равна',
+        'action': 'cube_area_result'
+    }
+
+context_volume = {
+        'title': 'Найти объем ' + FIGURE,
+        'h1': 'Объем ' + FIGURE,
+        'h2': H2,
+        'h3': 'Объем ' + FIGURE + ' равен',
+        'action': 'cube_volume_result'
+    }
+
 
 @router.get("/area/", response_class=HTMLResponse, name='cube_area')
 def area(request: Request):
 
     # Получить данные
     return templates.TemplateResponse(
-        request=request, name="geometry/cube_area.html", context={"similar_pages": []}
-    )
+        request=request, name="geometry/cube.html", context=context_area)
 
 
 @router.get("/area_result/", response_class=HTMLResponse, name='cube_area_result')
 async def area_result(request: Request, a: float):
     new_cube = Cube(a=a)
     result = new_cube.get_area()
+    context_area['result'] = result
+
     # Получить данные
     return templates.TemplateResponse(
-        request=request, name="geometry/cube_area.html", context={"result": result}
-    )
+        request=request, name="geometry/cube.html", context=context_area)
 
 
 @router.get("/volume/", response_class=HTMLResponse, name='cube_volume')
@@ -31,15 +50,14 @@ def volume(request: Request):
 
     # Получить данные
     return templates.TemplateResponse(
-        request=request, name="geometry/cube_vol.html", context={"similar_pages": []}
-    )
+        request=request, name="geometry/cube.html", context=context_volume)
 
 
 @router.get("/volume_result/", response_class=HTMLResponse, name='cube_volume_result')
 async def volume_result(request: Request, a: float):
     new_cube = Cube(a=a)
     result = new_cube.get_volume()
+    context_volume['result'] = result
     # Получить данные
     return templates.TemplateResponse(
-        request=request, name="geometry/cube_vol.html", context={"result": result}
-    )
+        request=request, name="geometry/cube.html", context=context_volume)
--- geometry/rhombus.py
+++ geometry/rhombus.py
@@ -1,3 +1,4 @@
+from fastapi.openapi.models import Schema
 from starlette.responses import HTMLResponse
 from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
@@ -8,39 +9,61 @@ router = APIRouter(prefix='/rhombus', tags=['Rhombus'])
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get("/area/", response_class=HTMLResponse, name='rhombus_area')
-def area(request: Request):
+class RhombusApi(Schema):
 
-    # Получить данные
-    return templates.TemplateResponse(
-        request=request, name="geometry/rhombus_area.html", context={"similar_pages": []}
-    )
+    figure: str = 'ромба'
+    context: dict
 
+    @router.get("/area/", response_class=HTMLResponse, name='rhombus_area')
+    def area(self, request: Request):
+        self.context.update(
+            {'title': 'Найти площадь ' + self.figure,
+                'h1': 'Площадь ' + self.figure,
+                'h2': 'найти через сторону и высоту',
+                'h3': 'Площадь ' + self.figure + ' равна',
+                'action': 'rhombus_area_result',
+                'main_form': """
+                            <label class="form-label" for="a">Введите сторону <b>(A)</b></label>
+                            <input type="number" class="form-control" id="a" name="a" step="any" required>
+            
+                            <label class="form-label" for="h">Введите высоту <b>(H)</b></label>
+                            <input type="number" class="form-control" id="h" name="h" step="any" required>
+                            """})
 
-@router.get("/area_result/", response_class=HTMLResponse, name='rhombus_area_result')
-async def area_result(request: Request, a: float, h: float):
-    new_rec = Rhombus(a=a, h=h)
-    result = new_rec.get_area()
-    # Получить данные
-    return templates.TemplateResponse(
-        request=request, name="geometry/rhombus_area.html", context={"result": result}
-    )
+        # Получить данные
+        return templates.TemplateResponse(
+            request=request, name="geometry/base.html", context=self.context)
 
+    @router.get("/area_result/", response_class=HTMLResponse, name='rhombus_area_result')
+    async def area_result(self, request: Request, a: float, h: float):
+        new_rec = Rhombus(a=a, h=h)
+        result = new_rec.get_area()
+        self.context['result'] = result
+        # Получить данные
+        return templates.TemplateResponse(
+            request=request, name="geometry/base.html", context=self.context)
 
-@router.get("/perimeter/", response_class=HTMLResponse, name='rhombus_perimeter')
-def perimeter(request: Request):
+    @router.get("/perimeter/", response_class=HTMLResponse, name='rhombus_perimeter')
+    def perimeter(self, request: Request):
+        self.context.update(
+            {'title': 'Найти периметр ' + self.figure,
+             'h1': 'Периметр ' + self.figure,
+             'h2': 'найти через сторону',
+             'h3': 'Периметр ' + self.figure + ' равен',
+             'action': 'rhombus_perimeter_result',
+             'main_form': """<label class="form-label" for="a">Введите сторону <b>(A)</b></label>
+                                <input type="number" class="form-control" id="a" name="a" step="any" required>"""
+             })
 
-    # Получить данные
-    return templates.TemplateResponse(
-        request=request, name="geometry/rhombus_perimeter.html", context={"similar_pages": []}
-    )
+        # Получить данные
+        return templates.TemplateResponse(
+            request=request, name="geometry/base.html", context=self.context)
 
-
-@router.get("/perimeter_result/", response_class=HTMLResponse, name='rhombus_perimeter_result')
-async def perimeter_result(request: Request, a: float):
-    new_rec = Rhombus(a=a)
-    result = new_rec.get_perimeter()
-    # Получить данные
-    return templates.TemplateResponse(
-        request=request, name="geometry/rhombus_perimeter.html", context={"result": result}
-    )
+    @router.get("/perimeter_result/", response_class=HTMLResponse, name='rhombus_perimeter_result')
+    async def perimeter_result(self, request: Request, a: float):
+        new_rec = Rhombus(a=a)
+        result = new_rec.get_perimeter()
+        self.context['result'] = result
+        # Получить данные
+        return templates.TemplateResponse(
+            request=request, name="geometry/base.html", context=self.context)
